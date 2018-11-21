95fc41a8ff9cc4a82cdbd30ade62d36f20e8e203
rorp24 <robinelzeard@gmail.com>
2018-11-21 16:45:11 +0100

    schéma d'entrée + pages manquantes

---
+++ app/src/main/java/com/example/frozen/coach/Choix.java
@@ -0,0 +1,13 @@
+package com.example.frozen.coach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Choix extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix);
+    }
+}
