703742f5296eb31b6bf2af1b5c53b5960fd81ea5
Johan <johan73460@outlook.fr>
2018-11-29 17:14:50 +0100

    boutton

--- app/src/main/java/com/example/frozen/coach/Choix.java
+++ app/src/main/java/com/example/frozen/coach/Choix.java
@@ -1,7 +1,10 @@
 package com.example.frozen.coach;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Choix extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Choix extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix);
+
+        final Button button = findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/frozen/coach/Recap.java
+++ app/src/main/java/com/example/frozen/coach/Recap.java
@@ -1,11 +1,13 @@
 package com.example.frozen.coach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Recap extends AppCompatActivity {
 
@@ -16,7 +18,24 @@ public class Recap extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Button button = findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Recap.this, MauvaiseHabitude.class);
+                startActivity(intent);
+            }
+        });
+        final Button button2 = findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Recap.this, Choix.class);
+                startActivity(intent2);
+            }
+        });
     }
 
 }
