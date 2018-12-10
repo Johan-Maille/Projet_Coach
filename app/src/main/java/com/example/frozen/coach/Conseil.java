61c4ac9447ad82bdd2900fb12544227695cf0938
Johan <johan73460@outlook.fr>
2018-12-10 16:58:20 +0100

    init

--- app/src/main/java/com/example/frozen/coach/Conseil.java
+++ app/src/main/java/com/example/frozen/coach/Conseil.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.PrintStreamPrinter;
 import android.widget.TextView;
 
@@ -113,8 +114,9 @@ public String affiche (String... params){
         e.printStackTrace();
         res = e.toString();
     }
-    return res;
 
+    return res;
+   // Log.i(res);
 }
 
 }
