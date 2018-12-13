9a2a00f9ee6f407097ba6eb6fa1a6110b9ee356c
Johan <johan73460@outlook.fr>
2018-12-13 14:02:48 +0100

    Liens button choix

--- app/src/main/java/com/example/frozen/coach/Choix.java
+++ app/src/main/java/com/example/frozen/coach/Choix.java
@@ -22,6 +22,73 @@ public class Choix extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button button8 = findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+
+
+            }
+        });
+        Button button9 = findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+
+
+            }
+        });
+        Button button10 = findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+
+
+            }
+        });
+        Button button6 = findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+
+
+            }
+        });
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+
+
+            }
+        });
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Choix.this, Formulaire.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
 
